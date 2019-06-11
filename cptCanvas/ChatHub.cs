02bedd60775d2f7e5adeff221c7ffd98e35b7627
mrAppTastic <henrikbeske@gmail.com>
2019-06-11 22:10:07 +0200

    Removed squareBandit and added new logic.

--- cptCanvas/ChatHub.cs
+++ cptCanvas/ChatHub.cs
@@ -5,9 +5,9 @@ namespace cptCanvas
 {
     public class ChatHub : Hub
     {
-        public void SendCoordinates(int x, int y, int s, string z)
+        public void SendCoordinates(int x, int y, int w, int h, string f, string s, int l, string m, string o, int u, int z, int q)
         {
-            Clients.All.receiveCoordinates(x, y, s, z);
+            Clients.All.receiveCoordinates(x, y, w, h, f, s, l, m, o, u, z, q);
         }
 
         public void Send(string name, string message, string toast)
